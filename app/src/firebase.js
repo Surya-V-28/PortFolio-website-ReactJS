e989ba4f07a5d9197fdfa5b0179365a27e2a2213
Surya-V-28 <suryaarmy231@gmail.com>
2023-05-27 13:55:09 +0530

    init commit

---
+++ app/src/firebase.js
@@ -0,0 +1,24 @@
+
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+// Import the functions you need from the SDKs you need
+
+
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+ 
+};
+
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+// const app = firebaseApp.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+export default db;
